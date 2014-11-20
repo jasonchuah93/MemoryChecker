7386327c2a8a4a8a7206f6c18ce17e96d7b1abb6
jasonchuah93 <jasonchuah1993@hotmail.com>
2014-11-20 20:22:05 +0800

    Test content whether is A

--- src/safeMalloc.c
+++ src/safeMalloc.c
@@ -9,22 +9,18 @@
 
 void *_safeMalloc(unsigned int size,int lineNumber, char *fileName){
 	int i ;
-    char *firstPtr,*lastPtr,*userPtr,content[256],tempContent ='5';
+    char *firstPtr,*lastPtr,*userPtr;
     printf("Requested to allocate %d size of memory\n and called "
 			"from %s at %d line number\n",size,fileName,lineNumber);
     void *memory = malloc(sizeof(620));
-   if(size<=620-size){
-        firstPtr = memory+256;
+        *((char*)memory)='5';
         for(i=0;i<256;i++){
-            content[i]=tempContent;
-            if(tempContent=='5')
-                tempContent = 'A';
+            if(*((char*)memory)=='5')
+                *((char*)memory) = 'A';
             else
-                tempContent = '5';
+                *((char*)memory) = '5';
         }
-        firstPtr = content;
-    }
-    
+    return memory;
 }
 
 void safeFree(void *memoryToFree){
--- test/test_safeMalloc.c
+++ test/test_safeMalloc.c
@@ -13,9 +13,10 @@ void tearDown(void){}
 
 void test_safeMalloc_should_allocate_the_memory_size_before_the_user_input(void){
     int i;
-    safeMalloc(100);
-    
-    
+    char *ptr = safeMalloc(100);
+    for(i=0;i<128;i++){
+        TEST_ASSERT_EQUAL('5',*(ptr));
+    }
 }
 
 void test_safeFree_should_free_all_memory(void){
