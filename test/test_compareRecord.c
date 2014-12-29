4db122712b516cffe6ecf6195649bd62b399bf56
jasonchuah93 <jasonchuah1993@hotmail.com>
2014-12-29 10:23:00 +0800

    Prepare to print even thought dint manage to finish

--- test/test_compareRecord.c
+++ test/test_compareRecord.c
@@ -5,16 +5,13 @@
 
 //Global variable
 int compare=0;
-
 void setUp(void){}
-
 void tearDown(void){}
 
 void test_findRecordCompare_should_return_1_if_recordInRedBlackTree_is_larger_than_targetMemory(void){
 	Record record = {.memory = (void*)100};
     Node *node = (Node*)&record;
     char *targetMemory = (void*)50;
-    
     compare = findRecordCompare(&node,targetMemory);
     TEST_ASSERT_EQUAL(1,compare);
 }
@@ -23,7 +20,6 @@ void test_findRecordCompare_should_return_negative_1_if_recordInRedBlackTree_is_
 	Record record = {.memory = (void*)100};
     Node *node = (Node*)&record;
     char *targetMemory = (void*)150;
-    
     compare = findRecordCompare(&node,targetMemory);
     TEST_ASSERT_EQUAL(-1,compare);
 }    
@@ -32,7 +28,6 @@ void test_findRecordCompare_should_return_0_if_recordInRedBlackTree_is_equal_to_
     Record record = {.memory = (void*)100};
     Node *node = (Node*)&record;
     char *targetMemory = (void*)100;
-    
     compare = findRecordCompare(&node,targetMemory);
     TEST_ASSERT_EQUAL(0,compare);
 }
@@ -41,7 +36,6 @@ void test_addAndDelRecordCompare_should_return_1_if_recordInRedBlackTree_is_larg
     Record recordFromRedBlackTree = {.memory = (void*)300};
     Record targetRecord = {.memory = (void*)200};
     Node *node = (Node*)&recordFromRedBlackTree;
-    
     compare = addAndDelRecordCompare(&node,&targetRecord);
     TEST_ASSERT_EQUAL(1,compare);
 }
@@ -50,7 +44,6 @@ void test_addAndDelRecordCompare_should_return_negative_1_if_recordInRedBlackTre
     Record recordFromRedBlackTree = {.memory = (void*)100};
     Record targetRecord = {.memory = (void*)200};
     Node *node = (Node*)&recordFromRedBlackTree;
-    
     compare = addAndDelRecordCompare(&node,&targetRecord);
     TEST_ASSERT_EQUAL(-1,compare);
 }
@@ -59,7 +52,6 @@ void test_addAndDelRecordCompare_should_return_0_if_recordInRedBlackTree_is_equa
     Record recordFromRedBlackTree = {.memory = (void*)200};
     Record targetRecord = {.memory = (void*)200};
     Node *node = (Node*)&recordFromRedBlackTree;
-    
     compare = addAndDelRecordCompare(&node,&targetRecord);
     TEST_ASSERT_EQUAL(0,compare);
 }
--- test/test_memoryAllocator.c
+++
@@ -1,7 +0,0 @@
-#include "unity.h"
-#include "memoryAllocator.h"
-
-void setUp(void){}
-
-void tearDown(void){}
-
